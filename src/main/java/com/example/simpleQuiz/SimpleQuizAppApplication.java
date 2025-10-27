a5754a590b817985a29f031b58ca0c5d806b0996
Victor Manin <victormanin151@gmail.com>
2025-10-28 00:06:47 +0200

    Initial commit

---
+++ src/main/java/com/example/simpleQuiz/SimpleQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleQuizAppApplication.class, args);
+	}
+
+}
