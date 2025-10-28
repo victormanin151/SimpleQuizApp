d0f79fadde4b0a7f507b0c2abcf55c1ebe1cb8c5
V Manin <victormanin151@gmail.com>
2025-10-28 09:30:04 +0200

    added a random question to quiz service

--- src/main/java/com/example/simpleQuiz/service/QuizService.java
+++ src/main/java/com/example/simpleQuiz/service/QuizService.java
@@ -9,6 +9,32 @@ import java.util.List;
 @Service
 public class QuizService {
     private List<Quiz> questions = new ArrayList<Quiz>();
+    private int currId = 1;
+
+    public QuizService() {
+        // Add sample questions with separate options
+        questions.add(new Quiz(currId++,
+                "What is Java?",
+                "Programming Language",
+                "Coffee",
+                "Island",
+                "Car",
+                "A",
+                false));
+
+        questions.add(new Quiz(currId++,
+                "Which planet is known as the Red Planet?",
+                "Earth",
+                "Mars",
+                "Venus",
+                "Jupiter",
+                "B",
+                false));
+    }
+
+    public List<Quiz> getAllQuestions() {
+        return questions;
+    }
 
 
 }
