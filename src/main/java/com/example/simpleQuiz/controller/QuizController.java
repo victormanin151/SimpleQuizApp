d8e2010bcccd11ae4d8a2de3596db86f209bcd79
V Manin <victormanin151@gmail.com>
2025-10-28 09:29:37 +0200

    changed the controller a bit

--- src/main/java/com/example/simpleQuiz/controller/QuizController.java
+++ src/main/java/com/example/simpleQuiz/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.example.simpleQuiz.controller;
 
+import com.example.simpleQuiz.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class QuizController {
 
+    private final QuizService service;
+
+    @Autowired
+    public QuizController(QuizService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
     public String index(Model model){
-
+        //model.addAttribute("Quiz",));
+        model.addAttribute("Quiz", service.getAllQuestions());
         return "index";
     }
 }
